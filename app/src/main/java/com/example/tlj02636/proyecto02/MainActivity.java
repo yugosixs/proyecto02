f9fb45458ba0508f3c6b50828919193603a7e153
yugosixs <34457170+yugosixs@users.noreply.github.com>
2017-12-12 12:45:49 -0600

    intial commit

---
+++ app/src/main/java/com/example/tlj02636/proyecto02/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tlj02636.proyecto02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edt1;
+    private EditText edt2;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt1=(EditText)findViewById(R.id.edt1);
+        edt2=(EditText)findViewById(R.id.edt2);
+        tv1=(TextView)findViewById(R.id.tv1);
+    } Tlajomulco.Iso2017
+    public void sumar (View view
+}
